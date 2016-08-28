f442335e072137c4b0b9f0657c59d9ef5a8473c7
bjoren92 <bjoren92@gmail.com>
2016-08-28 22:13:02 +0200

    Created the log functionality

--- app/app.js
+++ app/app.js
@@ -1,9 +1,31 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+angular.module('GameEngine', [])
+
+.controller('GameCtrl', ['$scope', 'logService', function ($scope, logService) {
+
+	$scope.getLog = function() {
+		return logService.getLog();
+	}
+}])
+
+.service('logService', [function () {
+	var MAX_LOG_ENTRIES = 50;
+	this.log = [{message: 'test', type: 'alarm'}];
+
+	this.addEntry = function(entryMessage, entryType) {
+		if(this.log.push({message: entryMessage, type: entryType}) > MAX_LOG_ENTRIES) {
+			this.log.shift();
+		}
+	}
+
+	this.getLog = function() {
+		this.scrollDown();
+		return this.log;
+	}
+
+	this.scrollDown = function() {
+		var element = document.getElementById("logContainer");
+		element.scrollTop = element.scrollHeight;
+	}
 }]);
